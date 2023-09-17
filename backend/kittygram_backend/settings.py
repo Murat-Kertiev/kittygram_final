99f6eb788bc5fb5b427cf11f2989a8f52f6c8950
Кертиев Мурат <kertiev1107gmail.com>
2023-09-17 15:29:42 +0300

    add actions

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'photogram.photo.org', '127.0.0.1', '158.160.66.161']
+ALLOWED_HOSTS = ['localhost', 'photogram.hoto.org', '127.0.0.1', '158.160.66.161']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
